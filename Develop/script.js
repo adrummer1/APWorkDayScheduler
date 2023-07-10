4aed9df9c23d49d76cdbe54bcca0af9770e232b9
adrummer1 <andrew@bluestemcreativeco.com>
2023-07-10 16:11:40 -0500

    Added function to change background color of descriptions based on past, present, or future.

--- Develop/script.js
+++ Develop/script.js
@@ -21,5 +21,52 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+// Global variables
+var TimeBlockEl = document.querySelector('.container-fluid');
 
+
+//Add current day, date, and time to header
 $('#currentDay').text(dayjs().format('dddd, MMM DD, YYYY hh:mm A'));
+
+//Event Listener for saveBtn clock
+$('.saveBtn').on('click', function() {
+  var text = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+
+  localStorage.setItem(time, text);
+});
+
+// Get item from local storage if there is anything.
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+function trackTask() {
+  var currentHour = dayjs().hour();
+
+  $('.time-block').each(function () {
+    var timeID = parseInt($(this).attr('id').split("hour")[1]);
+
+    if (timeID < currentHour) {
+      $(this).addClass('past');
+    }
+    else if (timeID === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });  
+}
+
+trackTask();
